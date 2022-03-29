c669d2182290ffc3aba1053e40bd42714a6acb6f
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-03-29 12:37:51 +0900

    add: Kids container next to social media icons

--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -9,12 +9,11 @@ function SocialLinks() {
 			<SocialLink image='https://static-mh.content.disney.io/starwars/assets/shared/icon_tumblr-9dbf2f5872a3.svg' />
 			<SocialLink image='https://static-mh.content.disney.io/starwars/assets/shared/icon_twitter-bde9a7f5abaa.svg' />
 			<SocialLink image='https://static-mh.content.disney.io/starwars/assets/shared/icon_youtube-ed78c6ee1c7d.svg' />
-
-			<ChildContainer>
+			<KidsContainer>
 				<a href=''>
 					<span />
 				</a>
-			</ChildContainer>
+			</KidsContainer>
 		</LinksContainer>
 	)
 }
@@ -27,4 +26,42 @@ const LinksContainer = styled.div`
 	gap: 20px;
 `
 
-const ChildContainer = styled.div``
+const KidsContainer = styled.div`
+  position: relative;
+	margin-left: 10px;
+
+	::before { 
+		content: "",
+		position: absolute;
+		border-top: 1px solid #343434;
+		top: -7px;
+		width: 1px;
+		height: 28px;
+		background: #343434;
+		right: 55px;
+	}
+	> a {
+		width: 43px;
+		height: 15px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background: #7a7a7a;
+		border-radius: 16px;
+		transition: background-color 350ms ;
+	}
+
+	> a > span {
+		width: 31px;
+		height: 9px;
+	  background-repeat: no-repeat;
+		background-image: url("https://static-mh.content.disney.io/starwars/assets/shared/icon_kids-dc39fc54f6c2.svg");
+    }  
+
+
+		:hover > a{
+			background-color: #ccc;
+		}
+	}
+
+`
